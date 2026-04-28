1635e33e5900f27d31868b4d6b2d9ff2090db76b
maria-khomynets <masha.khomynets@gmail.com>
2026-04-28 12:34:27 +0000

    Deploying to gh-pages from @ maria-khomynets/goit-js-hw-11@3ae5ffcc1c5b2ff0c0d01f060bf74ebb8d870eb6 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,2 +1,8 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */fetch("https://jsonp4laceholder.typicode.com/photos").then(o=>o.json()).then(o=>{console.log(o)}).catch(o=>{console.log(o.message)});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const n=document.querySelector(".image-list");function i(t){return fetch(t).then(e=>{if(!e.ok)throw new Error("Error");return e.json()})}function c(t){n.innerHTML=t.map(({thumbnailUrl:e,title:o,url:r})=>`
+      <li class="gallery-item">
+        <a href="${r}">
+          <img src="${e}" alt="${o}" />
+        </a>
+      </li>
+    `).join("")}i("https://jsonplaceholder.typicode.com/photos?_limit=10").then(t=>{c(t)}).catch(t=>console.log(t.message));
 //# sourceMappingURL=1-timer.js.map
