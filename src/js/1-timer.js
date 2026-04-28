3ae5ffcc1c5b2ff0c0d01f060bf74ebb8d870eb6
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-28 15:34:06 +0300

    practic

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,10 +1,28 @@
-fetch('https://jsonp4laceholder.typicode.com/photos')
-  .then(resp => {
+const listIm = document.querySelector('.image-list');
+
+function fetchData(url) {
+  return fetch(url).then(resp => {
+    if (!resp.ok) throw new Error('Error');
     return resp.json();
-  })
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.log(err.message);
   });
+}
+
+function render(arr) {
+  listIm.innerHTML = arr
+    .map(
+      ({ thumbnailUrl, title, url }) => `
+      <li class="gallery-item">
+        <a href="${url}">
+          <img src="${thumbnailUrl}" alt="${title}" />
+        </a>
+      </li>
+    `
+    )
+    .join('');
+}
+
+fetchData('https://jsonplaceholder.typicode.com/photos?_limit=10')
+  .then(res => {
+    render(res); // 👈 ОЦЕ важливо
+  })
+  .catch(err => console.log(err.message));
