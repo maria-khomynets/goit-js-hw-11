05c5d5106bfd64fc5849f0fe1166b9d8d654e2d1
Maria Khomynets <masha.khomynets@gmail.com>
2026-04-30 09:32:51 +0300

    test

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,28 +1,85 @@
-const listIm = document.querySelector('.image-list');
+// Потрібно створити функціонал для отримання прогнозу погоди в місті.
+// Використай публічне API https://www.weatherapi.com/docs/
+// Використовуй ендпоінт Forecast для того, щоб отримати прогноз погоди на декілька днів.
 
-function fetchData(url) {
-  return fetch(url).then(resp => {
-    if (!resp.ok) throw new Error('Error');
-    return resp.json();
+// Створи форму в яку користувач:
+// 1 вводить назву міста.
+// 2 обирає на яку кількість днів він хоче отримати прогноз погоди (3, 5 та 7 днів).
+// (Іноді параметр не працює в такому випадку можна зробити пошук на 1, 2 та 3 дні)
+// Приклад форми https://prnt.sc/kFmLOj6gBdv-
+
+// Після сабміту форми відмалюй картки з інформацією отриманою з бекенду.
+// Картка має містити відомості про:
+// 1 Зображення з погодою (icon)
+// 2 Текст з погодою (text)
+// 3 Дату (date)
+// 4 Середню температуру в Цельсія (avgtemp_c)
+// Приклад картки https://prnt.sc/h_p-A6Hty-i-
+
+// °C
+
+const BASE_URL = 'http://api.weatherapi.com/v1';
+const API_KEY = '6410346f89264d6e919165208231505';
+
+const form = document.querySelector('.js-search-form');
+const list = document.querySelector('.js-list');
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) {
+  event.preventDefault();
+
+  // const { city, days } = event.currentTarget.elements;
+  const cityValue = event.currentTarget.elements.city.value;
+  const daysValue = event.currentTarget.elements.days.value;
+
+  serviceWeather(cityValue, daysValue)
+    .then(data => {
+      console.log(data);
+      list.innerHTML = createMarkup(data.forecast.forecastday);
+    })
+    .catch(error => {
+      list.innerHTML = 'Ooops';
+    })
+    .finally(() => {
+      event.target.reset();
+    });
+}
+
+function serviceWeather(city = '', days = 1) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: city,
+    days: days,
+    lang: 'uk',
+  });
+
+  return fetch(`${BASE_URL}/forecast.json?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
   });
 }
 
-function render(arr) {
-  listIm.innerHTML = arr
+function createMarkup(arr) {
+  return arr
     .map(
-      ({ thumbnailUrl, title, url }) => `
-      <li class="gallery-item">
-        <a href="${url}">
-          <img src="${thumbnailUrl}" alt="${title}" />
-        </a>
-      </li>
+      ({
+        date,
+        day: {
+          avgtemp_c,
+          condition: { text, icon },
+        },
+      }) => `
+        <li class="weather-card">
+            <img src="${icon}" alt="${text}" class="weather-icon"/>
+            <h2 class="weather-date">${date}</h2>
+            <h3 class="weather-text">${text}</h3>
+            <h3 class="temperature">${avgtemp_c}°C</h3>
+        </li>
     `
     )
     .join('');
 }
-
-fetchData('https://jsonplaceholder.typicode.com/photos?_limit=10')
-  .then(res => {
-    render(res); // 👈 ОЦЕ важливо
-  })
-  .catch(err => console.log(err.message));
