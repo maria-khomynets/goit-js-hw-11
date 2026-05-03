021424f0862975d2889ccba5455da818cce1bdb1
Maria Khomynets <masha.khomynets@gmail.com>
2026-05-03 10:29:15 +0300

    import/expoprt

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
-export default function getImagesByQuery(query) {
-  const parameters = {
-    params: {
-      key: '55656452-b960420c5594901b542f03c34',
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-    },
-  };
-
+export function getImagesByQuery(query) {
   return axios
-    .get('https://pixabay.com/api/', parameters)
-    .then(response => response.data);
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: '55656452-b960420c5594901b542f03c34',
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(res => res.data);
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import getImagesByQuery from './js/pixabay-api';
+import { getImagesByQuery } from './js/pixabay-api';
 import {
   createGallery,
   clearGallery,
