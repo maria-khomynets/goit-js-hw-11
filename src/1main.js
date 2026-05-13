b1fb75e28e455340d1ec8934750e043c309b2966
Maria Khomynets <masha.khomynets@gmail.com>
2026-05-13 15:08:25 +0300

    commit

---
+++ src/1main.js
@@ -0,0 +1,9 @@
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
