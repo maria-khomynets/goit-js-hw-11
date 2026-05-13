f965f3b2089c80e1c71db68a8809ebce2d451aff
Maria Khomynets <masha.khomynets@gmail.com>
2026-05-13 15:02:08 +0300

    main

--- src/main.js
+++ src/main.js
@@ -1,64 +1,65 @@
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-// import { getImagesByQuery } from './js/pixabay-api';
-// import {
-//   createGallery,
-//   clearGallery,
-//   showLoader,
-//   hideLoader,
-// } from './js/render-functions';
+import axios from 'axios';
 
-// const form = document.querySelector('.form');
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
 
-// form.addEventListener('submit', handleSubmit);
+const form = document.querySelector('.form');
 
-// function handleSubmit(event) {
-//   event.preventDefault();
+form.addEventListener('submit', handleSubmit);
 
-//   const searchText = event.target.elements['search-text'].value.trim();
+function handleSubmit(event) {
+  event.preventDefault();
 
-//   if (!searchText) {
-//     iziToast.show({
-//       title: 'Warning',
-//       message: 'The form field must be filled in.',
-//       backgroundColor: 'pink',
-//       position: 'topLeft',
-//       timeout: 8000,
-//     });
-//     return;
-//   }
+  const searchText = event.target.elements['search-text'].value.trim();
 
-//   clearGallery();
+  if (!searchText) {
+    iziToast.show({
+      title: 'Warning',
+      message: 'The form field must be filled in.',
+      backgroundColor: 'pink',
+      position: 'topLeft',
+      timeout: 8000,
+    });
+    return;
+  }
 
-//   showLoader();
+  clearGallery();
 
-//   getImagesByQuery(searchText)
-//     .then(response => {
-//       if (response?.hits?.length) {
-//         createGallery(response.hits);
-//       } else {
-//         iziToast.show({
-//           title: 'Warning',
-//           message:
-//             'Sorry, there are no images matching your search query. Please try again!',
-//           backgroundColor: 'pink',
-//           position: 'topLeft',
-//           timeout: 8000,
-//         });
-//       }
+  showLoader();
 
-//       event.target.reset();
-//     })
-//     .catch(error => {
-//       iziToast.error({
-//         message: error.message,
-//         position: 'topLeft',
-//         timeout: 8000,
-//       });
-//     })
-//     .finally(() => {
-//       hideLoader();
-//     });
-// }
+  getImagesByQuery(searchText)
+    .then(response => {
+      if (response?.hits?.length) {
+        createGallery(response.hits);
+      } else {
+        iziToast.show({
+          title: 'Warning',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          backgroundColor: 'pink',
+          position: 'topLeft',
+          timeout: 8000,
+        });
+      }
 
+      event.target.reset();
+    })
+    .catch(error => {
+      iziToast.error({
+        message: error.message,
+        position: 'topLeft',
+        timeout: 8000,
+      });
+    })
+    .finally(() => {
+      hideLoader();
+    });
+}
